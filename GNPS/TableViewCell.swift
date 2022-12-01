908c99149b731c4d9bd05112f4afce11d5ceb8fb
Sammi Sprague <samantha.sprague23@d155.org>
2022-12-01 07:40:42 -0600

    initial commit: ui set up

---
+++ GNPS/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  GNPS
+//
+//  Created by SAMANTHA SPRAGUE on 12/1/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GNPS/ViewControllerAdd.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerAdd.swift
+//  GNPS
+//
+//  Created by SAMANTHA SPRAGUE on 12/1/22.
+//
+
+import UIKit
+
+class ViewControllerAdd: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
